72dd22499c277135b1a26f1fa475c2f2c0875521
David Whitney <codifyamplify@gmail.com>
2022-11-04 12:20:12 -0400

    commit tree 3e89928e3b3a364578c3d204268adf9bfa4a10b0

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,29 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, onSelectCategory}) {
+  // console.log(categories)
+  // create a variable set to a mapped array of categories, creating a button for each one
+  const categoryButtons = categories.map(
+    (category) => {
+      const className = category === selectedCategory ? "selected" : null
+        return (
+          <button
+            key={category}
+            className = {className}
+            onClick={() => {
+              onSelectCategory(category)
+              console.log(category + " it is then!")}
+            }>
+              {category}
+          </button>
+        )
+    }
+  )
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onTaskFormSubmit}) {
+  const [text, setText] = useState("")
+  const [category, setCategory] = useState("Code")
+// console.log(categories + " from newtaskform component")
+
+  function handleSubmit(event){
+    event.preventDefault();
+    onTaskFormSubmit({ text, category });
+    setText("")
+    setCategory("Code")
+  }
+// console.log(category)
+console.log(text)
   return (
-    <form className="new-task-form">
+ <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          {categories.map((cat) => (
+            <option key={cat}>{cat}</option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
